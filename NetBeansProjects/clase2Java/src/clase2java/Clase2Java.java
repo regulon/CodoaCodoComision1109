8f6325d6d9fae35e08960b59696f75d00336ad23
regulon <regulolunagit config --global user.email reguloluna>
2021-11-02 12:14:08 -0300

    Se agrego un nuevo archivo al IntegradorJs

---
+++ NetBeansProjects/clase2Java/src/clase2java/Clase2Java.java
@@ -0,0 +1,29 @@
+
+package clase2java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Regulo R Luna Aponte
+ */
+public class Clase2Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese una contraseña");
+        String password = scanner.nextLine();
+                
+        if("Regulo".equals(nombre)){
+            System.out.println("Bienvenido + nombre");      
+        }else{
+            
+        }
+    }
+    
+}
